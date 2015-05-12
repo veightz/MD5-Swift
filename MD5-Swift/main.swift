9903e892c9b1eb7caa4570ec986eb5c3e9240fc4
veightz <veightz@gmail.com>
2015-05-12 11:33:58 +0800

    Initial Commit

---
+++ MD5-Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MD5-Swift
+//
+//  Created by Veight Zhou on 5/12/15.
+//  Copyright (c) 2015 Veight Zhou. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
